d28173a847b22a18c4fbf5b4b3b54b657d30b31a
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-11-23 16:33:08 +0100

    adds menu animation and backdrop

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,12 +1,25 @@
 const sidebar = document.querySelector("#sidebar");
 const menuBtn = document.querySelector("#menu-btn");
+const menuBackdrop = document.querySelector("#backdrop");
+
+// const { screen } = require("../../tailwind.config.js");
 
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("show-sidebar");
   if (sidebar.className.includes("-translate-x-[200%]")) {
     sidebar.classList.remove("-translate-x-[200%]");
+    menuBackdrop.classList.remove("hidden");
+    menuBackdrop.classList.add("fixed");
+    console.log("show");
   } else {
     sidebar.classList.add("-translate-x-[200%]");
-    console.log("no");
+    menuBackdrop.classList.remove("fixed");
+    menuBackdrop.classList.add("hidden");
+    console.log("hidden");
   }
+});
+
+window.addEventListener("resize", () => {
+  //   console.log(window.innerWidth);
+  //   console.log(screen);
 });
