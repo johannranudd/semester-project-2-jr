5ef067f22cd25a655d73128d232d68b33c3e1a57
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-11-27 18:04:34 +0100

    adds popular listings section

--- src/js/listings/listings.mjs
+++ src/js/listings/listings.mjs
@@ -5,6 +5,98 @@ import { loadingSpinner, removeSpinner } from "../utils/loading.mjs";
 
 const listingsULElement = document.querySelector("#listing");
 
+async function filterTwentyMostPopular() {
+  loadingSpinner(listingsULElement);
+  const data = await getListings();
+
+  const sortedByMostBids = sortByMostBids(data);
+
+  const stillForSale = getListingsStillForSale(sortedByMostBids);
+
+  const listingWithCountdownObject = addCountdownObject(stillForSale);
+
+  // limit to 20
+  const twentyMostPopular = listingWithCountdownObject.slice(0, 10);
+  displayListings(twentyMostPopular, listingsULElement);
+}
+
+function displayListings(data, listElement, isAddingToPrevList = false) {
+  if (!isAddingToPrevList) {
+    if (listElement) {
+      listElement.innerHTML = "";
+      loadingSpinner(listingsULElement);
+    }
+  }
+
+  if (data) {
+    removeSpinner();
+    data.map((listing) => {
+      const {
+        id,
+        title,
+        media,
+        seller,
+        bids,
+        _count,
+        endsAt,
+        countDownObject,
+        description,
+      } = listing;
+      // const { days, hours, minutes, seconds } = countDownObject;
+      const highestBid = filterHighestBid(listing);
+      // const countDownString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      console.log(listing);
+      const listItem = ` 
+             <div class="card">
+                    <a href="#" class="image-container">
+                         <img src="${
+                           media[0]
+                             ? media[0]
+                             : "../../../assets/images/placeholder.png"
+                         }" alt="Lsiting image" />
+                    </a>
+              <div class="text-content">
+                <div class="title-and-price">
+                  <h5 class="title">${title}</h5>
+                  <p class="price">$ <span> ${highestBid}</span></p>
+                </div>
+                <p class="seller">${seller.name}</p>
+                  <p class="description">${
+                    description ? description : "No description"
+                  }</p>
+              </div>
+            </div>
+          `;
+      listElement.innerHTML += listItem;
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", filterTwentyMostPopular);
+
+// sort by most bids
+function sortByMostBids(data) {
+  return data.sort((a, b) => {
+    if (a.bids.length < b.bids.length) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+// get listing still for sale
+function getListingsStillForSale(data) {
+  return data.filter((listing) => {
+    if (listing.bids.length > 0) {
+      const timeLeft = returnsTimeLeftInt(listing);
+      if (timeLeft > 0) {
+        return listing;
+      }
+    }
+  });
+}
+
 function calculateTime(timeLeft) {
   const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
@@ -22,44 +114,6 @@ function returnsTimeLeftInt(listing) {
   return countDownDate - now;
 }
 
-async function filterTwentyMostPopular() {
-  loadingSpinner(listingsULElement);
-  const data = await getListings();
-
-  // sort by most bids
-  const sortedByMostBids = data.sort((a, b) => {
-    if (a.bids.length < b.bids.length) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-
-  // get listing still for sale
-  const stillForSale = sortedByMostBids.filter((listing) => {
-    if (listing.bids.length > 0) {
-      const timeLeft = returnsTimeLeftInt(listing);
-      if (timeLeft > 0) {
-        return listing;
-      }
-    }
-  });
-
-  // modify listings still fro sale
-  const modifiedStillForSale = stillForSale.map((listing) => {
-    const timeLeft = returnsTimeLeftInt(listing);
-    const countDownObject = calculateTime(timeLeft);
-    return {
-      ...listing,
-      countDownObject,
-    };
-  });
-
-  // limit to 20
-  const twentyMostPopular = modifiedStillForSale.slice(0, 10);
-  displayListings(data, listingsULElement);
-}
-
 function filterHighestBid(listings) {
   const highestBid = listings.bids.reduce((accumulator, currentValue) => {
     accumulator.push(currentValue.amount);
@@ -68,87 +122,14 @@ function filterHighestBid(listings) {
   return Math.max(...highestBid);
 }
 
-function displayListings(data, list, isAddingToPrevList = false) {
-  if (!isAddingToPrevList) {
-    if (list) {
-      list.innerHTML = "";
-      loadingSpinner(listingsULElement);
-    }
-  }
-
-  if (data) {
-    removeSpinner();
-    // !test
-    data.map((listings) => {
-      const { id, media, seller, bids, _count, endsAt, countDownObject } =
-        listings;
-      // const { days, hours, minutes, seconds } = countDownObject;
-      // const highestBid = filterHighestBid(listings);
-      // const countDownString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-      // const allMedia = media.map((image) => {
-      // return image;
-      // });
-      // console.log(media);
-      const listItem = ` 
-             <div class="card">
-                    <a href="#" class="image-container">
-                        <img
-                        class=""
-                        src="${media.map((image) => {
-                          return image;
-                        })}"
-                          alt=""
-                          />
-                  </a>
-              <div class="text-content">
-                  <h5>
-                    ${seller.name}
-                  </h5>
-                  <p>
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
-                  </p>
-              </div>
-            </div>
-          `;
-      listingsULElement.innerHTML += listItem;
-    });
-    // !test
-    // data.map((listings) => {
-    //   const { id, media, seller, bids, _count, endsAt, countDownObject } =
-    //     listings;
-    //   const { days, hours, minutes, seconds } = countDownObject;
-    //   const highestBid = filterHighestBid(listings);
-    //   const countDownString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-    //   const allMedia = media.map((image) => {
-    //     return image;
-    //   });
-    //   // console.log(media);
-    //   const listItem = ` <div class="flex justify-center">
-    //         <div class="rounded-lg shadow-lg bg-white max-w-sm">
-    //           <a href="#">
-    //             <img
-    //               class="rounded-t-lg"
-    //               src="${media.map((image) => {
-    //                 return image;
-    //               })}"
-    //               alt=""
-    //             />
-    //           </a>
-    //           <div class="p-6">
-    //             <h5 class="text-gray-900 text-xl font-medium mb-2">
-    //               ${seller.name}
-    //             </h5>
-    //             <p class="text-gray-700 text-base mb-4">
-    //               Some quick example text to build on the card title and make up
-    //               the bulk of the card's content.
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>`;
-    //   list.innerHTML += listItem;
-    // });
-  }
+// modify listings still fro sale (give it a countdown object)
+function addCountdownObject(data) {
+  return data.map((listing) => {
+    const timeLeft = returnsTimeLeftInt(listing);
+    const countDownObject = calculateTime(timeLeft);
+    return {
+      ...listing,
+      countDownObject,
+    };
+  });
 }
-
-window.addEventListener("DOMContentLoaded", filterTwentyMostPopular);
